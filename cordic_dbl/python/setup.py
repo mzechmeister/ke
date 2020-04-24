55faa93bafe4c64d968b849739a7bb9264139662
Mathias Zechmeister <zechmeister@astro.physik.uni-goettingen.de>
2020-04-24 20:33:40 +0200

    Added ke_dbl_f compilation and suppress parentheses warning.

--- cordic_dbl/python/setup.py
+++ cordic_dbl/python/setup.py
@@ -3,5 +3,9 @@
 from setuptools import setup, Extension
 
 setup(name='ke_dbl_i',
-      ext_modules=[Extension('lib.ke_dbl_i', ['lib/ke_dbl_i.c'])]
+      ext_modules=[Extension('lib.ke_dbl_i', ['lib/ke_dbl_i.c'], extra_compile_args=['-Wno-parentheses'])]
+     )
+
+setup(name='ke_dbl_f',
+      ext_modules=[Extension('lib.ke_dbl_f', ['lib/ke_dbl_f.c'])]
      )
