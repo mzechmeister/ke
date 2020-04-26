50f8e71b003ca57645912836c8f51b73b14b2dbe
Mathias Zechmeister <zechmeister@astro.physik.uni-goettingen.de>
2020-04-26 17:32:30 +0200

    Support for setup install.

--- cordic_dbl/python/setup.py
+++ cordic_dbl/python/setup.py
@@ -3,9 +3,11 @@
 from setuptools import setup, Extension
 
 setup(name='ke_dbl_i',
+      py_modules = ['ke_dbl_i'],
       ext_modules=[Extension('lib.ke_dbl_i', ['lib/ke_dbl_i.c'], extra_compile_args=['-Wno-parentheses'])]
      )
 
 setup(name='ke_dbl_f',
+      py_modules = ['ke_dbl_f'],
       ext_modules=[Extension('lib.ke_dbl_f', ['lib/ke_dbl_f.c'])]
      )
