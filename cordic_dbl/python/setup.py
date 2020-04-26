8fc9e57de1f67d9e7d5a04243cb2e3a6e7f890c8
Mathias Zechmeister <zechmeister@astro.physik.uni-goettingen.de>
2020-04-27 00:09:52 +0200

    Fix #3

--- cordic_dbl/python/setup.py
+++ cordic_dbl/python/setup.py
@@ -8,12 +8,12 @@ setup(name='ke_dbl_i',
       py_modules=['ke_dbl_i'],
       ext_modules=[Extension('lib.ke_dbl_i', ['lib/ke_dbl_i.c'],
                                 extra_compile_args=['-Wno-parentheses', '-pie'],
-                                extra_link_args=['-lm', '-pie'])]
+                                extra_link_args=['-lm', '-pie', '-Wl,-E'])]
      )
 
 setup(name='ke_dbl_f',
       py_modules=['ke_dbl_f'],
       ext_modules=[Extension('lib.ke_dbl_f', ['lib/ke_dbl_f.c'],
                                 extra_compile_args=['-pie'],
-                                extra_link_args=['-lm', '-pie'])]
+                                extra_link_args=['-lm', '-pie', '-Wl,-E'])]
      )
