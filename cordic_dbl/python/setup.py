b0914e60b1b4fc2ff9f62666f3957f9c4d503b67
Mathias Zechmeister <zechmeister@astro.physik.uni-goettingen.de>
2020-04-27 00:23:35 +0200

    refactor: Same package name ke_dbl

--- cordic_dbl/python/setup.py
+++ cordic_dbl/python/setup.py
@@ -4,16 +4,13 @@ from setuptools import setup, Extension
 
 # with options -pie and -lm the shared library is also executable
 
-setup(name='ke_dbl_i',
-      py_modules=['ke_dbl_i'],
+setup(name='ke_dbl',
+      py_modules=['ke_dbl_i', 'ke_dbl_f'],
       ext_modules=[Extension('lib.ke_dbl_i', ['lib/ke_dbl_i.c'],
                                 extra_compile_args=['-Wno-parentheses', '-pie'],
-                                extra_link_args=['-lm', '-pie', '-Wl,-E'])]
-     )
-
-setup(name='ke_dbl_f',
-      py_modules=['ke_dbl_f'],
-      ext_modules=[Extension('lib.ke_dbl_f', ['lib/ke_dbl_f.c'],
+                                extra_link_args=['-lm', '-pie', '-Wl,-E']),
+                   Extension('lib.ke_dbl_f', ['lib/ke_dbl_f.c'],
                                 extra_compile_args=['-pie'],
-                                extra_link_args=['-lm', '-pie', '-Wl,-E'])]
+                                extra_link_args=['-lm', '-pie', '-Wl,-E'])
+                  ]
      )
