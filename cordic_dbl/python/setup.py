09dd61c69d5ae94764a6758193a164776aacf128
Mathias Zechmeister <zechmeister@astro.physik.uni-goettingen.de>
2020-04-26 23:22:14 +0200

    feat: Executable shared library

--- cordic_dbl/python/setup.py
+++ cordic_dbl/python/setup.py
@@ -2,12 +2,18 @@
 
 from setuptools import setup, Extension
 
+# with options -pie and -lm the shared library is also executable
+
 setup(name='ke_dbl_i',
-      py_modules = ['ke_dbl_i'],
-      ext_modules=[Extension('lib.ke_dbl_i', ['lib/ke_dbl_i.c'], extra_compile_args=['-Wno-parentheses'])]
+      py_modules=['ke_dbl_i'],
+      ext_modules=[Extension('lib.ke_dbl_i', ['lib/ke_dbl_i.c'],
+                                extra_compile_args=['-Wno-parentheses', '-pie'],
+                                extra_link_args=['-lm', '-pie'])]
      )
 
 setup(name='ke_dbl_f',
-      py_modules = ['ke_dbl_f'],
-      ext_modules=[Extension('lib.ke_dbl_f', ['lib/ke_dbl_f.c'])]
+      py_modules=['ke_dbl_f'],
+      ext_modules=[Extension('lib.ke_dbl_f', ['lib/ke_dbl_f.c'],
+                                extra_compile_args=['-pie'],
+                                extra_link_args=['-lm', '-pie'])]
      )
